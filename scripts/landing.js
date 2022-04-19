6e2d8d8ded97e88eb5d51129ee0e97164bb48e70
Waed Hafitha <waed@kiitos-tech.com>
2022-04-20 01:59:25 +0300

    landing page js functionality, and products data

---
+++ scripts/landing.js
@@ -0,0 +1,126 @@
+import productsData from "./productsData.js";
+
+const initialProducts = [...productsData];
+let productsContainer = document.getElementById("products");
+
+// categories
+let categories = new Set();
+let categoriesList = document.getElementById("categories-list");
+
+//filters: display, price
+let displayGrid = document.getElementById("grid");
+let displayList = document.getElementById("list");
+let priceHighToLow = document.getElementById("highToLow");
+let priceLowToHigh = document.getElementById("lowToHigh");
+
+// search: button, input
+let searchBtn = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-input");
+
+// cart: number of added products || and Add to cart Btn
+let addedProductsCount = document.getElementById("added-products-count");
+let addToCartBtn = document.getElementsByClassName("add-cart-btn");
+
+// Display products function
+const displayProducts = (products) => {
+  productsContainer.innerHTML = "";
+
+  products.forEach((el) => {
+    let productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    let productImg = document.createElement("img");
+    productImg.setAttribute("src", el.imgSrc);
+
+    let productPrice = document.createElement("span");
+    productPrice.classList.add("product-price");
+    productPrice.innerText = `$ ${el.price}`;
+
+    let productName = document.createElement("span");
+    productName.classList.add("product-name");
+    productName.innerText = el.name;
+
+    let addProductBtn = document.createElement("button");
+    addProductBtn.classList.add("add-cart-btn");
+    addProductBtn.innerText = "Add to Cart";
+
+    productDiv.appendChild(productImg);
+    productDiv.appendChild(productPrice);
+    productDiv.appendChild(productName);
+    productDiv.appendChild(addProductBtn);
+
+    productsContainer.appendChild(productDiv);
+    categories.add(el.category);
+  });
+
+  // Add categories to categories list
+  categoriesList.innerHTML = "";
+  categories.forEach((el) => {
+    let li = document.createElement("li");
+    li.innerText = el;
+    categoriesList.appendChild(li);
+  });
+};
+
+displayProducts(initialProducts);
+
+// Display & price
+displayGrid.addEventListener("click", () => {
+  productsContainer.style.flexDirection = "row";
+});
+
+displayList.addEventListener("click", () => {
+  productsContainer.style.flexDirection = "column";
+});
+
+priceLowToHigh.addEventListener("click", () => {
+  let products = [...initialProducts];
+  products.sort((a, b) => parseInt(a.price) - parseInt(b.price));
+
+  displayProducts(products);
+});
+
+priceHighToLow.addEventListener("click", () => {
+  let products = [...initialProducts];
+  products.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+
+  displayProducts(products);
+});
+
+// Search button event: click
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value.length == 0) {
+    searchInput.focus();
+  } else {
+    let filteredProducts = filterProducts(searchInput.value);
+    displayProducts(filteredProducts);
+  }
+});
+
+searchInput.addEventListener("input", (e) => {
+  let filteredProducts = filterProducts(e.target.value);
+  displayProducts(filteredProducts);
+});
+
+const filterProducts = (searchInput) => {
+  return initialProducts.filter((el) => {
+    return el.category
+      .toLocaleLowerCase()
+      .includes(searchInput.toLocaleLowerCase());
+  });
+};
+
+// Filter by categories
+categoriesList.addEventListener("click", (e) => {
+  let clickedCategory = e.target.innerText.toLocaleLowerCase();
+  let filteredProducts = filterProducts(clickedCategory);
+
+  displayProducts(filteredProducts);
+});
+
+// Add to Cart btn
+for (let i = 0; i < addToCartBtn.length; i++) {
+  addToCartBtn[i].addEventListener("click", (e) => {
+    addedProductsCount.innerText = parseInt(addedProductsCount.innerText) + 1;
+  });
+}
---
+++ scripts/productsData.js
@@ -0,0 +1,125 @@
+// product: photo, price, name, category
+let productsData = [
+  {
+    imgSrc: "./images/camera1.jpg",
+    price: "65",
+    name: "Video Camera Camcorder",
+    category: "Cameras",
+  },
+  {
+    imgSrc: "./images/headphone1.jpg",
+    price: "10",
+    name: "Sony ZX Series Wired On-Ear Headphones",
+    category: "Headphones",
+  },
+  {
+    imgSrc: "./images/laptop1.jpg",
+    price: "1600",
+    name: "Lenovo IdeaPad 3",
+    category: "Laptops",
+  },
+  {
+    imgSrc: "./images/phone1.jpg",
+    price: "160",
+    name: "Tracfone Blu View 4G LTE Prepaid",
+    category: "SmartPhones",
+  },
+  {
+    imgSrc: "./images/camera2.jpg",
+    price: "1800",
+    name: "Samsung WB35F",
+    category: "Cameras",
+  },
+  {
+    imgSrc: "./images/headphone2.jpg",
+    price: "127",
+    name: "Beats Studio3 Wireless Noise Cancelling",
+    category: "Headphones",
+  },
+  {
+    imgSrc: "./images/laptop2.jpg",
+    price: "480",
+    name: "Lenovo IdeaPad 3 Laptop",
+    category: "Laptops",
+  },
+  {
+    imgSrc: "./images/phone2.jpg",
+    price: "1000",
+    name: "Samsung Galaxy S21 FE",
+    category: "SmartPhones",
+  },
+  {
+    imgSrc: "./images/camera3.jpg",
+    price: "800",
+    name: "DMC-FZ2000",
+    category: "Cameras",
+  },
+  {
+    imgSrc: "./images/headphone3.jpg",
+    price: "15",
+    name: "Kids Headphones",
+    category: "Headphones",
+  },
+  {
+    imgSrc: "./images/laptop3.jpg",
+    price: "262",
+    name: "ASUS Laptop L5",
+    category: "Laptops",
+  },
+  {
+    imgSrc: "./images/phone3.jpg",
+    price: "300",
+    name: "Motorola one 5G ACE",
+    category: "SmartPhones",
+  },
+  {
+    imgSrc: "./images/camera4.jpg",
+    price: "320",
+    name: "LUMIx FZ82",
+    category: "Cameras",
+  },
+  {
+    imgSrc: "./images/headphone4.jpg",
+    price: "180",
+    name: "Apple Aripods",
+    category: "Headphones",
+  },
+  {
+    imgSrc: "./images/laptop4.jpg",
+    price: "1000",
+    name: "Touch screen laptop",
+    category: "Laptops",
+  },
+  {
+    imgSrc: "./images/phone4.jpg",
+    price: "160",
+    name: "Tracfone Samsung Galaxy A12",
+    category: "SmartPhones",
+  },
+  {
+    imgSrc: "./images/camera5.jpg",
+    price: "800",
+    name: "Sony Alpha a6000",
+    category: "Cameras",
+  },
+  {
+    imgSrc: "./images/headphone5.jpg",
+    price: "32",
+    name: "Artix CL750 Wired Headphones",
+    category: "Headphones",
+  },
+  {
+    imgSrc: "./images/laptop5.jpg",
+    price: "100",
+    name: "HP  Chromebook",
+    category: "Laptops",
+  },
+  {
+    imgSrc: "./images/phone5.jpg",
+    price: "160",
+    name: "Nokia G10",
+    category: "SmartPhones",
+  },
+];
+
+export default productsData;
